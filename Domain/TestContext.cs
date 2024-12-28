80885700abb9e04a30ba2b46ffa032b0b81cead0
m-karlin <m.karlin@dodopizza.com>
2024-12-28 09:15:16 +0300

    200

--- Domain/TestContext.cs
+++ Domain/TestContext.cs
@@ -3,5 +3,6 @@ namespace Sandbox.Domain;
 public class SagaContext
 {
     public string? Id { get; set; }
-    public string? BaseActivityId { get; set; }
+    public string? StoredTraceId { get; set; }
+    public string? StoredTraceState { get; set; }
 }
--- Grains/TestGrain.cs
+++ Grains/TestGrain.cs
@@ -13,7 +13,7 @@ public interface ITestGrain : IGrainWithStringKey
 public class TestGrain : Grain, ITestGrain, IRemindable
 {
     private SagaContext? _sagaContext;
-    private readonly string _key;
+    private readonly string _grainKey;
     private static readonly TimeSpan WakeUpReminderPeriod = TimeSpan.FromMinutes(1);
     private readonly SagaContextRepository _sagaContextRepository;
     private readonly ILogger<TestGrain> _logger;
@@ -22,7 +22,7 @@ public class TestGrain : Grain, ITestGrain, IRemindable
     {
         _sagaContextRepository = contextRepository;
         _logger = logger;
-        _key = this.GetPrimaryKeyString();
+        _grainKey = this.GetPrimaryKeyString();
     }
 
     public override async Task OnActivateAsync(CancellationToken cancellationToken)
@@ -34,17 +34,33 @@ public class TestGrain : Grain, ITestGrain, IRemindable
             WakeUpReminderPeriod,
             WakeUpReminderPeriod);
 
-        _sagaContext = await _sagaContextRepository.Get(_key, cancellationToken);
+        _sagaContext = await _sagaContextRepository.Get(_grainKey, cancellationToken);
         if (_sagaContext != null)
         {
+            ActivityContext? activityContext = null!;
+            if (ActivityContext.TryParse(
+                _sagaContext.StoredTraceId,
+                _sagaContext.StoredTraceState,
+                out var ctx))
+            {
+                activityContext = ctx;
+            }
+
             using var activity = Activity.Current?.Source.StartActivity(
-                name: "SagaWakeUp", 
-                kind: ActivityKind.Internal, 
-                // Установка связи
-                parentId: _sagaContext.BaseActivityId);
+                name: "SagaWakeUp",
+                kind: ActivityKind.Internal,
+                activityContext ?? Activity.Current.Context
+            );
+
+            await SomeAction();
 
             LogMessages.SagaContinued(_logger);
         }
+    }
+
+    public Task SomeAction()
+    {
+        return Task.CompletedTask;
     }
 
     public async Task StartSaga(CancellationToken cancellationToken)
@@ -53,9 +69,9 @@ public class TestGrain : Grain, ITestGrain, IRemindable
         LogMessages.SagaStarted(_logger);
         _sagaContext = new SagaContext
         {
-            // Сохранение id активити
-            BaseActivityId = activity?.Id,
-            Id = _key
+            Id = _grainKey,
+            StoredTraceId = activity?.Id,
+            StoredTraceState = activity?.TraceStateString
         };
         await _sagaContextRepository.Set(_sagaContext, cancellationToken);
     }
